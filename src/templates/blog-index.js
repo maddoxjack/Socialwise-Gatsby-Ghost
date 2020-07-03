9fd590068e6680adef047895a1cd7b5b5d87e9b3
Jack Maddox <jackdmaddox@gmail.com>
2020-07-03 14:28:42 +0100

    added landing page index, updated urls to include blog

--- src/templates/blog-index.js
+++ src/templates/blog-index.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import React from "react"
+import PropTypes from "prop-types"
+import { graphql } from "gatsby"
 
-import { Layout, PostCard, Pagination } from '../components/common'
-import { MetaData } from '../components/common/meta'
+import { Layout, PostCard, Pagination } from "../components/common"
+import { MetaData } from "../components/common/meta"
 
 /**
-* Main index page (home page)
-*
-* Loads all posts from Ghost and uses pagination to navigate through them.
-* The number of posts that should appear per page can be setup
-* in /utils/siteConfig.js under `postsPerPage`.
-*
-*/
+ * Main index page (home page)
+ *
+ * Loads all posts from Ghost and uses pagination to navigate through them.
+ * The number of posts that should appear per page can be setup
+ * in /utils/siteConfig.js under `postsPerPage`.
+ *
+ */
 const Index = ({ data, location, pageContext }) => {
+    console.log(`this is data`, data)
     const posts = data.allGhostPost.edges
 
     return (
@@ -49,17 +50,17 @@ export default Index
 // This page query loads all posts sorted descending by published date
 // The `limit` and `skip` values are used for pagination
 export const pageQuery = graphql`
-  query GhostPostQuery($limit: Int!, $skip: Int!) {
-    allGhostPost(
-        sort: { order: DESC, fields: [published_at] },
-        limit: $limit,
-        skip: $skip
-    ) {
-      edges {
-        node {
-          ...GhostPostFields
+    query GhostPostQuery($limit: Int!, $skip: Int!) {
+        allGhostPost(
+            sort: { order: DESC, fields: [published_at] }
+            limit: $limit
+            skip: $skip
+        ) {
+            edges {
+                node {
+                    ...GhostPostFields
+                }
+            }
         }
-      }
     }
-  }
 `
