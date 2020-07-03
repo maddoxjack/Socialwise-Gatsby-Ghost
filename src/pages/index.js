2c152acb47ea1ba47da19cb56afe7611062857ac
Jack Maddox <jackdmaddox@gmail.com>
2020-07-03 15:07:02 +0100

    add link to landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Landing = () => (
     <>
-        <h1>Landing</h1>
+        <h1>This will be the Landing Page</h1>
+        <Link to="/blog">
+            <h1>This is the link to Blog</h1>
+        </Link>
     </>
 )
 
