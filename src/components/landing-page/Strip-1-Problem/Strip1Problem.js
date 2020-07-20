fbd0ad4b6c34113d93ba47f072a3b867ebc33b7a
Jack Maddox <jackdmaddox@gmail.com>
2020-07-20 09:48:52 +0100

    strip 1 updates

--- src/components/landing-page/Strip-1-Problem/Strip1Problem.js
+++ src/components/landing-page/Strip-1-Problem/Strip1Problem.js
@@ -1,29 +1,35 @@
 import React from 'react'
 
 const StripOne = () => (
-    <>
-        <h2>
-        You can&apos;t find your Twitter community and it&apos;s starting to get
-        frustrating.
-        </h2>
-        <>
+    <section id="strip-one">
+        <div>
+            <h2>
+            You can&apos;t find your Twitter community and it&apos;s starting to
+            get frustrating.
+            </h2>
             <h3>Socialwise Benefits:</h3>
-        </>
-        <ul>
-            <li>✅ No need to learn any complicated tools. We’ve pre-vetted the users and built the searches.
-            </li>
-            <li>✅ We use proprietary search algorithms to pre-vet users talking about things that matter to you.
-
-            </li>
-            <li>✅ Our setup prevents us from being a noise amplifier and to show only relevant communities.
-
-            </li>
-            <li>✅ Our setup prevents us from being a noise amplifier and to show only relevant communities.
-
-            </li>
-        </ul>
-        <h3>Focus on Growth. Not learning new tools.</h3>
-    </>
+            <div>
+                <ul>
+                    <li>
+                    ✅ No need to learn any complicated tools. We’ve pre-vetted
+                    the users and built the searches.
+                    </li>
+                    <li>
+                    ✅ We use proprietary search algorithms to pre-vet users
+                    talking about things that matter to you.
+                    </li>
+                    <li>
+                    ✅ Our setup prevents us from being a noise amplifier and to
+                    show only relevant communities.
+                    </li>
+                    <li>
+                    ✅ Our setup prevents us from being a noise amplifier and to
+                    show only relevant communities.
+                    </li>
+                </ul>
+            </div>
+            <h3>Focus on Growth. Not learning new tools.</h3>
+        </div>
+    </section>
 )
-
 export default StripOne
