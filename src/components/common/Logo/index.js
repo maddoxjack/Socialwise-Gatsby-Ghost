e1841c33163c32460acbe0313fe38914cd262e09
Jack Maddox <jackdmaddox@gmail.com>
2020-08-24 16:45:43 +0100

    increase logo heart size, adjust navbar item css in logo file

--- src/components/common/Logo/index.js
+++ src/components/common/Logo/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import logo from "../../../images/logo.svg"
 import "./styles.scss"
+import { Helmet } from "react-helmet"
+
 // import HeaderLogoImg from "../../landing-page/Images/HeaderLogo"
 
 // const Logo = () => (
@@ -10,19 +12,16 @@ import "./styles.scss"
 //     </div>
 // )
 
-const Logo = ({ size }) => (
-    <div id="logo" className={size}>
-        <img src={logo} alt="logo" /> <span>socialwise</span>
-    </div>
+const Logo = () => (
+    <>
+        <Helmet>
+            <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet"/>
+        </Helmet>
+        <div id="logo" >
+            <img src={logo} alt="logo" id="logo-heart"/> <span id="logo-text">socialwise</span>
+        </div>
+    </>
 )
-
-Logo.propTypes = {
-    size: PropTypes.string,
-}
-
-Logo.defaultProps = {
-    size: `full`,
-}
 
 export default Logo
 
