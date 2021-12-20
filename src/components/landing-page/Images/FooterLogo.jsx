d652dcc7ba2a217440aa5ede1bd12bd8f427972b
Jack Maddox <jackdmaddox@gmail.com>
2021-12-20 20:46:26 +0000

    renamed footer logo file

--- src/components/landing-page/Images/FooterLogo.jsx
+++ src/components/landing-page/Images/FooterLogo.jsx
@@ -6,7 +6,7 @@ export default function FooterLogoImg() {
     const data = useStaticQuery(
         graphql`
             query {
-                footerlogo: file(relativePath: { eq: "logoText.png" }) {
+                footerlogo: file(relativePath: { eq: "logo-text.png" }) {
                     childImageSharp {
                         # Specify the image processing specifications right in the query.
                         # Makes it trivial to update as your page's design changes.
